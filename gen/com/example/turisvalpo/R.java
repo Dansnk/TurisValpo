e000d318d84e1d5f6bc0e2ceae05ebbabb2d46cb
Dansnk <snk.styles@gmail.com>
2013-11-22 21:48:08 -0300

    Agregar Botones y Acciones

--- gen/com/example/turisvalpo/R.java
+++ gen/com/example/turisvalpo/R.java
@@ -31,12 +31,14 @@ public final class R {
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_localizacion=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_creditos=0x7f030000;
+        public static final int activity_localizacion=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
-        public static final int localizacion=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int creditos=0x7f070000;
+        public static final int localizacion=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
         public static final int Button1=0x7f050003;
@@ -46,6 +48,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int bienvenido=0x7f050002;
         public static final int hello_world=0x7f050007;
+        public static final int title_activity_creditos=0x7f050008;
         public static final int title_activity_localizacion=0x7f050006;
     }
     public static final class style {
--- src/com/example/turisvalpo/MainActivity.java
+++ src/com/example/turisvalpo/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.turisvalpo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +14,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Button Button1 = (Button) findViewById(R.id.button1);
+		final Button Button2 = (Button) findViewById(R.id.button2);
+		final Button Button3 = (Button) findViewById(R.id.button3);
+		
+		Button3.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, Creditos.class);
+				startActivity(intent);
+				
+			}
+		});
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
