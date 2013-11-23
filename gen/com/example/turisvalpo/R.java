146e263017a2661f2121f1b042638d4717462838
Antonio Cayulao <antonio.cayulao@feutfsm.cl>
2013-11-23 14:58:04 -0300

    actualizacion

--- gen/com/example/turisvalpo/R.java
+++ gen/com/example/turisvalpo/R.java
@@ -225,8 +225,9 @@ containing a value of this type.
     }
     public static final class layout {
         public static final int activity_creditos=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_vista_general=0x7f030002;
+        public static final int activity_localizacion=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_vista_general=0x7f030003;
     }
     public static final class menu {
         public static final int creditos=0x7f0a0000;
