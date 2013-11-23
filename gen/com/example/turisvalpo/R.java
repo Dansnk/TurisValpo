39c9625defc2f31db5ea298fff3e08502be8e279
Dansnk <snk.styles@gmail.com>
2013-11-22 21:01:23 -0300

    Botones Agregados

--- gen/com/example/turisvalpo/R.java
+++ gen/com/example/turisvalpo/R.java
@@ -24,18 +24,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f080002;
+        public static final int button3=0x7f080003;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_localizacion=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int localizacion=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int Button1=0x7f050003;
+        public static final int Button2=0x7f050004;
+        public static final int Button3=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int bienvenido=0x7f050002;
+        public static final int hello_world=0x7f050007;
+        public static final int title_activity_localizacion=0x7f050006;
     }
     public static final class style {
         /** 
